ca13df750b7d356ca2ddcdabf15138742ead95f1
胡文龙 <1925540204@qq.com>
2022-03-28 21:18:58 +0800

    HTTP客户端Feign 1. 引入依赖 2. 添加@EnableFeignClients注解 3. 编写FeignClient接口 4. 调用接口中的方法

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package cn.itcast.order.service;
 
+import cn.itcast.order.clients.UserClient;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.pojo.User;
@@ -13,19 +14,34 @@ public class OrderService {
     @Autowired
     private OrderMapper orderMapper;
     @Autowired
-    private RestTemplate restTemplate;
+    private UserClient userClient;
 
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
-        // 2. 使用RestTemplate发起Http请求
-//        String url = "http://localhost:8081/user/" + order.getUserId();
-        // 改成服务名称
-        String url = "http://userservice/user/" + order.getUserId();
-        User user = restTemplate.getForObject(url, User.class);
+        // 2. 使用Feign发起Http请求
+        User user = userClient.findById(order.getUserId());
         // 3. 封装user到order对象
         order.setUser(user);
         // 4.返回
         return order;
     }
+
+
+//    @Autowired
+//    private RestTemplate restTemplate;
+//
+//    public Order queryOrderById(Long orderId) {
+//        // 1.查询订单
+//        Order order = orderMapper.findById(orderId);
+//        // 2. 使用RestTemplate发起Http请求
+////        String url = "http://localhost:8081/user/" + order.getUserId();
+//        // 改成服务名称
+//        String url = "http://userservice/user/" + order.getUserId();
+//        User user = restTemplate.getForObject(url, User.class);
+//        // 3. 封装user到order对象
+//        order.setUser(user);
+//        // 4.返回
+//        return order;
+//    }
 }
